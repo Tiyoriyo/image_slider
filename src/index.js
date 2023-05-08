2b9f9f87950cd8aae6831f1e3e0fd17e8a1c33c5
Vadim Kostrov <vmkvo15@outlook.com>
2023-05-08 15:29:19 +0400

    Add project files

---
+++ src/index.js
@@ -0,0 +1,108 @@
+import './style.css';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
+import image5 from './images/image5.jpg';
+
+const body = document.querySelector('body');
+
+function createPictureStrip() {
+    const pictureStrip = document.createElement('div');
+    pictureStrip.classList.add('p-strip');
+
+    const img1 = new Image(375, 253);
+    const img2 = new Image(375, 253);
+    const img3 = new Image(375, 253);
+    const img4 = new Image(375, 253);
+    const img5 = new Image(375, 253);
+    img1.src = image1;
+    img2.src = image2;
+    img3.src = image3;
+    img4.src = image4;
+    img5.src = image5;
+    
+    pictureStrip.append(img1, img2, img3, img4, img5);
+    return pictureStrip;
+}
+
+function createNavDots() {
+    function addCircleMoveEvent() {
+        const navdots = container.children;
+        for (let i = 0; i < navdots.length; i += 1) {
+
+            let decrease = -375;
+            let fullDecrease = i * decrease;
+
+            navdots[i].addEventListener('click', () => {
+                const strip = document.querySelector('.p-strip');
+                strip.style.transform = `translate(${fullDecrease}px)`;
+                for (let j = 0; j < navdots.length; j += 1) {
+                    navdots[j].classList.remove('active');
+                }
+                navdots[i].classList.add('active');
+            });
+        }
+    }
+    const container = document.createElement('div');
+    container.classList.add('navbar');
+
+    for (let i = 0; i < 5; i += 1) {
+        const circle = document.createElement('div');
+        circle.classList.add('navdot');
+        container.append(circle);
+    }
+
+    addCircleMoveEvent();
+    console.log(container.children)
+
+    return container
+}
+
+function createPictureFrame() {
+    const element = document.createElement('div');
+    element.classList.add('frame');
+
+    element.append(createPictureStrip(), createNavDots());
+    return element;
+}
+
+let x = 0;
+
+function moveStrip(string) {
+    const strip = document.querySelector('.p-strip');
+    if (string == 'next' && x !== -1500) {
+        x -= 375;
+        console.log(x);
+    } else if (string == 'prev' && x !== 0) {
+        x += 375;
+        console.log(x);
+    }
+    strip.style.transform = `translate(${x}px)`
+}
+
+function createModule() {
+    const container = document.createElement('div');
+    container.classList.add('p-container');
+
+    const prevBtn = document.createElement('button');
+    const frame = createPictureFrame();
+    const nextBtn = document.createElement('button');
+
+    prevBtn.classList.add('p-prev');
+    nextBtn.classList.add('p-next');
+    prevBtn.innerHTML = '&#8592;';
+    nextBtn.innerHTML = '&#8594;';
+
+    nextBtn.addEventListener('click', () => {moveStrip('next')});
+
+    prevBtn.addEventListener('click', () => {moveStrip('prev')});
+
+    container.append(prevBtn, frame, nextBtn);
+    return container;
+}
+
+body.appendChild(createModule());
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Image Carousel',
+            template: './src/index.html'
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.jpg$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
